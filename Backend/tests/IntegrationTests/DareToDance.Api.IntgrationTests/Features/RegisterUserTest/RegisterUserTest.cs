53c9a4c5925bcb06e76cb766da3268ce7c9f4b00
tmadmin <tmadmin@TDL595>
2026-08-20 16:31:36 +0200

    Verify login code integration test

--- Backend/tests/IntegrationTests/DareToDance.Api.IntgrationTests/Features/RegisterUserTest/RegisterUserTest.cs
+++ Backend/tests/IntegrationTests/DareToDance.Api.IntgrationTests/Features/RegisterUserTest/RegisterUserTest.cs
@@ -3,7 +3,7 @@ using System.Net;
 using System.Net.Http.Json;
 using Xunit;
 
-namespace DareToDance.Api.IntgrationTests.Features;
+namespace DareToDance.Api.IntgrationTests.Features.RegisterUserTest;
 
 public class RegisterUserTest : IClassFixture<CustomWebApplicationFactory>
 {
@@ -25,9 +25,7 @@ public class RegisterUserTest : IClassFixture<CustomWebApplicationFactory>
             Phone = "123456789"
         };
 
-        var response = await _client.PostAsJsonAsync(
-            "/users/register",
-            request);
+        var response = await _client.PostAsJsonAsync("/users/register",request);
 
         Assert.Equal(HttpStatusCode.Created, response.StatusCode);
     }
