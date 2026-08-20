e743c89324b39f8495fb91983fd1ef98fef61559
tmadmin <tmadmin@TDL595>
2026-08-20 16:04:50 +0200

    Register integration test

---
+++ Backend/tests/IntegrationTests/DareToDance.Api.IntgrationTests/Features/RegisterUserTest.cs
@@ -0,0 +1,34 @@
+﻿using DareToDance.Api.IntgrationTests.Common;
+using System.Net;
+using System.Net.Http.Json;
+using Xunit;
+
+namespace DareToDance.Api.IntgrationTests.Features;
+
+public class RegisterUserTest : IClassFixture<CustomWebApplicationFactory>
+{
+    private readonly HttpClient _client;
+
+    public RegisterUserTest(CustomWebApplicationFactory factory)
+    {
+        _client = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task RegisterUser_Should_ReturnCreated()
+    {
+        var request = new
+        {
+            FirstName = "John",
+            LastName = "Doe",
+            Email = "john.doe@test.com",
+            Phone = "123456789"
+        };
+
+        var response = await _client.PostAsJsonAsync(
+            "/users/register",
+            request);
+
+        Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+    }
+}
