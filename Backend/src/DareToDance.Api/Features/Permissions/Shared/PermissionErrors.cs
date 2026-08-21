19591400a8d1dce76a7f91f8e392a0e6bab9c39a
tmadmin <tmadmin@TDL595>
2026-08-21 11:28:04 +0200

    CreatePermission

---
+++ Backend/src/DareToDance.Api/Features/Permissions/Shared/PermissionErrors.cs
@@ -0,0 +1,10 @@
+﻿using ErrorOr;
+
+namespace DareToDance.Api.Features.Permissions.Shared;
+
+public static class PermissionErrors
+{
+    public static readonly Error DuplicateName = Error.Conflict(
+        code: "User.DuplicateName",
+        description: "A permission with this name already exists.");
+}
